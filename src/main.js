c8e8a1c3433eda55d5b071cee83eff279a86d94e
giuliXtor <giulia.tortorella@outlook.it>
2025-01-28 11:10:26 +0100

    Test files

--- src/main.js
+++ src/main.js
@@ -6,7 +6,5 @@ import { createApp } from "vue"
 // Import of the App.vue file
 import App from "./App.vue"
 
-
-
 // Create Vue appliction with App.vue component and inject it into element with id 'app'
 createApp(App).mount("#app")
